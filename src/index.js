c64469913749513496f6c4c880e301449a2797a5
Aashish rana <aashishrana71135@gmail.com>
2023-11-23 13:19:40 +0530

    index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';                  // Index file
 import App from './App';
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux';
